65f21dd700ae706a5405e83232245718bdf616d3
brad <brad@brad.tw>
2017-04-15 10:34:21 +0800

    2017041501

---
+++ Java2003/src/tw/org/iii/java2003/Brad75.java
@@ -0,0 +1,40 @@
+package tw.org.iii.java2003;
+
+public class Brad75 {
+	public static void main(String[] args) {
+		MyThread mt1 = new MyThread("A");
+		MyThread mt2 = new MyThread("B");
+		mt1.start();
+		mt2.start();
+		try {
+			Thread.sleep(400);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Hello, World");
+		try {
+			Thread.sleep(20*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		mt1.run();
+
+	}
+}
+
+class MyThread extends Thread {
+	private String name;
+	MyThread(String name){this.name= name;}
+	@Override
+	public void run() {
+		for (int i=0; i<10; i++){
+			System.out.println(name + ":" + i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+			}
+		}
+	}
+}
